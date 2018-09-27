4b4db43f9b5e017615550863b14f80cb22962729
0jbaumstark <42749684+0jbaumstark@users.noreply.github.com>
2018-09-27 13:52:58 -0500

    Add files via upload

--- f1.js
+++ f1.js
@@ -33,8 +33,9 @@ function plusplus (b) {
 // (3) Third Task
 // Declare a function named oppRecip() which outputs the opposite reciprocal of the input number.
 
-function oppRecip ()
-
+function oppRecip (c\d) {
+    let total= d/c * c/d
+    return total
 
 
 
@@ -44,10 +45,10 @@ function oppRecip ()
 // Declare a function named pthagorean() which has two inputs you guessed it a and b.
 // The function outputs the value of the third side of the right triangle.
 
-function pthagorean (a,b) }
-    let total 
-
-
+function pthagorean (a,b) {
+    let total = c = a*a + b*b
+    return Math.sqrt (c)
+}
 
 
 
@@ -55,8 +56,10 @@ function pthagorean (a,b) }
 // Declare a function named circleArea() which has one input named radius.
 // The function outputs the area of the circle.
 
-
-
+function circleArea (radius) {
+    let total = 2 * radius * Math.PI
+    return total 
+}
 
 
 
@@ -65,8 +68,10 @@ function pthagorean (a,b) }
 // Declare a function named boxVolume() which has three inputs: length, width, and depth.
 // The function outputs the volume of the box;
 
-
-
+function boxVolume (l,w,d) {
+    let total = l*w*d
+    return total
+}
 
 
 
@@ -79,7 +84,11 @@ function pthagorean (a,b) }
 // Declare a function named thisIsMadness() which accepts one string input named city.
 // The function should output the string: "No! This is <city>!!"
 
-
+function thisIsMadness(city){
+    let sentence = "No! This is <city>!!"
+    return sentence
+}
+    
 
 
 
@@ -89,9 +98,11 @@ function pthagorean (a,b) }
 // Declare a function named yo() which accepts one string input named fam.
 // The function should output the string: "yo <fam>!  What's good <fam>??"
 
-
-
-
+function yo(fam) {
+    let friend = "Yo <fam>! What's good <fam>??"
+    return friend
+}
+    
 
 
 
@@ -99,7 +110,9 @@ function pthagorean (a,b) }
 // Declare a function named beefDay() which has no inputs.
 // This function should output a long string, IN ALL CAPS, of "BEEF DAY" with lots of extra E's, F's, A's, and Y's
 
-
+function beefDay(){
+    let thursdays = "BEEF DAY"
+    return thursdays
 
 
 
@@ -113,7 +126,10 @@ function pthagorean (a,b) }
 // Declare a function named xor() which has two inputs a and b.
 // This function returns the exclusive or of the two boolean inputs.
 
-
+function xor(a,b){
+    let total = (a || b) && !(a && b);
+    return total
+}
 
 
 
